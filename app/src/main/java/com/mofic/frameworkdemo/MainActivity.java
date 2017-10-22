1efe26943cc3bd0649276867ded90feb73029999
mofic <lwn314@163.com>
2017-10-22 15:58:29 +0800

    commit

---
+++ app/src/main/java/com/mofic/frameworkdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mofic.frameworkdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
